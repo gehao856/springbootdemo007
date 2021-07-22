90dd3a022754250c27550ac117f2fe687b37082f
gehao <1318329100@qq.com>
2021-07-22 17:39:26 +0800

    在分支gehao上增加了支付模块

--- MySpringBootDubboConsumer/src/main/java/com/openlab/controller/PayController.java
+++ MySpringBootDubboConsumer/src/main/java/com/openlab/controller/PayController.java
@@ -1,5 +1,6 @@
 package com.openlab.controller;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -7,5 +8,9 @@ public class PayController {
 
     public  String pay(String out_trade_no){
         return "success";
+    }
+
+    public void  demo001(String name){
+        System.out.println("geao");
     }
 }
