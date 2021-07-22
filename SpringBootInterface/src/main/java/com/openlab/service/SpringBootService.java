98f3f0704387cebf3f69151d98db2298a10b2e60
gehao856 <58731542+gehao856@users.noreply.github.com>
2021-07-22 16:12:43 +0800

    修改接口
    添加springboot接口

--- SpringBootInterface/src/main/java/com/openlab/service/SpringBootService.java
+++ SpringBootInterface/src/main/java/com/openlab/service/SpringBootService.java
@@ -3,4 +3,6 @@ package com.openlab.service;
 public interface SpringBootService {
 
     public String helloSpringBootDubbo(String name);
+    
+    public String SpringBootDubbo(String password);
 }
